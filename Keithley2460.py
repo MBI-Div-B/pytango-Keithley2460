2ed9adb81abd53fcaf5db7a275899ed01ecacc59
Michael Schneider <mschneid@mbi-berlin.de>
2022-02-01 13:27:25 +0100

    flake8, remove sys.exit on error

--- Keithley2460.py
+++ Keithley2460.py
@@ -22,88 +22,87 @@ import time
 import numpy as np
 
 
-
-
 class Keithley2460(Device):
 
     current = attribute(
-            name='current',
-            access=READ_WRITE,
-            unit='A',
-            dtype=tango.DevFloat,
-            format='%10.4f',
-            )
+        name="current",
+        access=READ_WRITE,
+        unit="A",
+        dtype=tango.DevFloat,
+        format="%10.4f",
+    )
 
     voltage = attribute(
-            name='voltage',
-            access=READ,
-            unit='V',
-            dtype=tango.DevFloat,
-            format='%10.4f',
-            )
+        name="voltage",
+        access=READ,
+        unit="V",
+        dtype=tango.DevFloat,
+        format="%10.4f",
+    )
 
     curr_hist = attribute(
-            name='curr_hist',
-            access=READ,
-            dtype=(float,),
-            max_dim_x=100000,
-            )
+        name="curr_hist",
+        access=READ,
+        dtype=(float,),
+        max_dim_x=100000,
+    )
 
     output = attribute(
-            name='output',
-            access=READ_WRITE,
-            dtype=tango.DevBoolean,
-            )
+        name="output",
+        access=READ_WRITE,
+        dtype=tango.DevBoolean,
+    )
 
     host = device_property(
-            dtype=str,
-            mandatory=True,
-            update_db=True,
-            )
+        dtype=str,
+        mandatory=True,
+        update_db=True,
+    )
 
     def init_device(self):
         Device.init_device(self)
-        self.rm = visa.ResourceManager('@py')
-        self.inst = self.rm.open_resource(f'TCPIP::{self.host}::INSTR')
+        self.rm = visa.ResourceManager("@py")
+        self.inst = self.rm.open_resource(f"TCPIP::{self.host}::INSTR")
         self.inst.clear()
         try:
             # self.inst.write('*RST')  # commented to maintain output state
-            self.inst.write('*CLS')
-            ans = self.inst.query('*IDN?')
+            self.inst.write("*CLS")
+            ans = self.inst.query("*IDN?")
             print(ans)
-            if 'MODEL 2460' in ans:
+            if "MODEL 2460" in ans:
                 self.source_setup()
                 self.set_state(DevState.ON)
+                self._current = 0
+                self._voltage = 0
+                self._output = True
+                self._setpoint = self.read_current()
+                self._history = [self._setpoint]
+                self.write_output(True)
             else:
+                print(f"Wrong device at address:\n{ans}", file=self.log_error)
                 self.set_state(DevState.FAULT)
-                sys.exit(255)
+                # sys.exit(255)
         except Exception as e:
-            print(e, file=self.error_stream)
-            self.inst.close()
+            print(e, file=self.log_error)
             self.set_state(DevState.FAULT)
-            sys.exit(255)
-        self._current = 0
-        self._voltage = 0
-        self._output = True
-        self._setpoint = self.read_current()
-        self._history = [self._setpoint]
-        self.write_output(True)
+            # self.inst.close()
+            # sys.exit(255)
 
     def always_executed_hook(self):
         msg = ':READ? "defbuffer1", READ, SOURCE, SOURSTATUS'
         ans = self.inst.query(msg)
-        print(f'always_executed_hook -> {ans}', file=self.log_debug)
+        print(f"always_executed_hook -> {ans}", file=self.log_debug)
         try:
-            voltage, current, status = ans.split(',')
+            voltage, current, status = ans.split(",")
             self._voltage = float(voltage)
             self._current = float(current)
             s = int(status)
             self._status = [s >> i & 1 for i in range(8)]
-            print(f'status: {self._status}', file=self.log_debug)
         except Exception as e:
-            # likely a timeout ocurred - flush buffer
-            print(f'unexpected {e}: {msg} -> {ans}. Clearing buffer.',
-                  file=self.log_warn)
+            # likely a timeout occurred - flush buffer
+            print(
+                f"unexpected {e}: {msg} -> {ans}. Clearing buffer.", file=self.log_warn
+            )
             self.inst.clear()
         return
 
@@ -122,29 +121,31 @@ class Keithley2460(Device):
     def write_current(self, value):
         if float(value) == 0:
             value = 1e-7
-        self.inst.write(f'SOUR:CURR {value:.8f}')
+        self.inst.write(f"SOUR:CURR {value:.8f}")
         self._history.append(value)
 
     def write_output(self, value):
-        val = 'ON' if value else 'OFF'
-        self.inst.write(f'OUTP {val}')
+        val = "ON" if value else "OFF"
+        self.inst.write(f"OUTP {val}")
 
     @command
     def clear_history(self):
-        self._history = [self._history[-1],]
+        self._history = [
+            self._history[-1],
+        ]
 
     def source_setup(self):
-        self.inst.write('ROUT:TERM FRON')  # use front terminal out
+        self.inst.write("ROUT:TERM FRON")  # use front terminal out
         self.inst.write('SENS:FUNC "VOLT"')
-        self.inst.write('SENS:VOLT:NPLC 1')
-        self.inst.write('SENS:VOLT:RANG:AUTO ON')
-        self.inst.write('SOUR:FUNC CURR')
-        self.inst.write('SOUR:CURR:READ:BACK 1')
-        self.inst.write('SOUR:CURR:VLIM 100')
+        self.inst.write("SENS:VOLT:NPLC 1")
+        self.inst.write("SENS:VOLT:RANG:AUTO ON")
+        self.inst.write("SOUR:FUNC CURR")
+        self.inst.write("SOUR:CURR:READ:BACK 1")
+        self.inst.write("SOUR:CURR:VLIM 100")
 
     @command
     def reset_device(self):
-        self.inst.write('*RST')
+        self.inst.write("*RST")
         self.source_setup()
         self.write_output(True)
 
